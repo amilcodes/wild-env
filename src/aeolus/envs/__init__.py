22c1145a3ddfc1401fce83fc0ba7920ae91d95d4
amilcodes <amil@brown.edu>
2026-06-27 17:20:00 -0400

    Expose PettingZoo and RLlib environment adapters

---
+++ src/aeolus/envs/__init__.py
@@ -0,0 +1,5 @@
+from .parallel import AeolusParallelEnv
+from .tensor_incident import TensorIncidentEnv
+from .tensor_operations import TensorOperationsEnv
+
+__all__ = ["AeolusParallelEnv", "TensorIncidentEnv", "TensorOperationsEnv"]
